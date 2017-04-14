96d45e933bf3be2c1dbb85c0ea250c6332f9b145
G016A1330\ukyou <togingham@gmail.com>
2017-04-14 09:45:47 +0900

    4/14 アイテムのぷれはぶ、画像、プログラム

---
+++ Assets/Suzuki/Item/Program/item.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item : MonoBehaviour {
+
+    public float i_speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(4.0f, 1.0f, 1.0f));
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        var tag = collision.gameObject.tag;
+        if (tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
